5669c2d79cd6ff7aba5e675520ccf9986f4030f2
ACSE-F5B790FE <shengshi.cao23@imperial.ac.uk>
2023-10-09 17:40:43 +0100

    <commit>

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,25 @@
 import numpy as np
 
+# 新的导入
+from scipy.ndimage import gaussian_filter
+from scipy import misc
 
-__all__ = ['rand_array']
+
+# 修改
+# __all__ = ['rand_array']
+# __all__ = ['rand_array', 'smooth_image']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 
 
 def rand_array(shape):
     return np.random.rand(*shape)
+
+
+# 添加的功能
+def smooth_image(a, sigma=1):
+    return gaussian_filter(a, sigma=sigma)
+
+
+# 添加的功能2
+def my_mat_solve(A, b):
+    return A.inv()*b
